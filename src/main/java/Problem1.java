e44ec1b9fea64f5a2b0be9eca21d2f40caf2e1b0
ST5906 <transebastian21@gmail.com>
2024-09-20 11:26:47 -0400

    null check

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,22 +1,36 @@
 // Problem1.java
 public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
-        ListNode origNode = l1;
-        ListNode node = new ListNode(l2.val);
-        ListNode copyNode = node;
-
-        while (l2.next != null){
-            l2 = l2.next;
-            node.next = new ListNode(l2.val);
-            node = node.next;
+        if (l2 == null){
+            return l1;
         }
-
-        while (l1.next != null){
-            l1 = l1.next;
+        else if (l1 == null){
+            ListNode node = new ListNode(l2.val);
+            ListNode copyNode = node;
+            while (l2.next != null){
+                l2 = l2.next;
+                node.next = new ListNode(l2.val);
+                node = node.next;
+            }
+            return copyNode;
         }
+        else{
+            ListNode node = new ListNode(l2.val);
+            ListNode copyNode = node;
+            while (l2.next != null){
+                l2 = l2.next;
+                node.next = new ListNode(l2.val);
+                node = node.next;
+            }
 
-        l1.next = copyNode;
+            ListNode origNode = l1;
+            while (l1.next != null){
+                l1 = l1.next;
+            }
 
-        return origNode;
+            l1.next = copyNode;
+
+            return origNode;
+        }
     }
 }

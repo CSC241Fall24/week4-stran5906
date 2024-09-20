6fff8d8682796caac5d631345c3fa92b00a3af3e
ST5906 <transebastian21@gmail.com>
2024-09-20 11:06:42 -0400

    bruh moment

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -3,6 +3,8 @@ public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
         ListNode origNode = l1;
         ListNode node = new ListNode(l2.val);
+        ListNode copyNode = node;
+
         while (l2.next != null){
             l2 = l2.next;
             node.next = new ListNode(l2.val);
@@ -13,7 +15,7 @@ public class Problem1 {
             l1 = l1.next;
         }
 
-        l1.next = node;
+        l1.next = copyNode;
 
         return origNode;
     }
